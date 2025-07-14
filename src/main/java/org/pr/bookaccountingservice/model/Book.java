4049864123417106b0fecfbf204fcc3b044ab94f
Alekseev George <alekseevg888@gmail.com>
2025-07-14 21:04:27 +0300

    add: models

--- src/main/java/org/pr/bookaccountingservice/model/Book.java
+++ src/main/java/org/pr/bookaccountingservice/model/Book.java
@@ -1,4 +1,23 @@
 package org.pr.bookaccountingservice.model;
 
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Data
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private int year;
+
+    private String genre;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
